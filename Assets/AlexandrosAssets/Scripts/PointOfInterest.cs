b4ce00dc71315d697a0376d009d4e5a30166bcae
Alexandros Tsompolis <alekostsom@gmail.com>
2018-02-22 00:45:13 +0200

    minor changes

--- Assets/AlexandrosAssets/Scripts/PointOfInterest.cs
+++ Assets/AlexandrosAssets/Scripts/PointOfInterest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mapbox.Unity.MeshGeneration.Factories;
@@ -8,6 +9,16 @@ public class PointOfInterest : MonoBehaviour {
 	public DirectionsFactory directionsFactory;
 	
 	public List<MapManager.PoiCategory> categories;
+	
+	public float latitude;
+	public float longitude;
+	
+	[Serializable]
+	 public struct CategoryWeight {
+		 public MapManager.PoiCategory category;
+		 public int weight; //(0-10)
+	 }
+	 public CategoryWeight[] weightssda;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +30,16 @@ public class PointOfInterest : MonoBehaviour {
 		}
 		
 		Debug.Log(polyCounter);
+		
+		weightssda = new CategoryWeight[8];
+		weightssda[0].category = MapManager.PoiCategory.Museum;
+		weightssda[1].category = MapManager.PoiCategory.Fortress;
+		weightssda[2].category = MapManager.PoiCategory.Religion;
+		weightssda[3].category = MapManager.PoiCategory.Lighthouse;
+		weightssda[4].category = MapManager.PoiCategory.Plaza;
+		weightssda[5].category = MapManager.PoiCategory.ArchaelogicalSite;
+		weightssda[6].category = MapManager.PoiCategory.ArchitecturalMonument;
+		weightssda[7].category = MapManager.PoiCategory.OldArtisanship;
 		
 	}
 	
